614cec09a67a7e490f501758be0a5086d5573905
Dan Pierce - linux box <dan@pierceclan.net>
2017-07-23 21:28:02 -0400

    First Commit

---
+++ src/AllTests.cpp
@@ -0,0 +1,15 @@
+/*
+ * AllTests.cpp
+ *
+ *  Created on: Jul 23, 2017
+ *      Author: dan
+ */
+
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int ac, char** av)
+{
+    return CommandLineTestRunner::RunAllTests(ac, av);
+}
+
---
+++ src/SodukuTests.cpp
@@ -0,0 +1,19 @@
+/*
+ * SodukuTests.cpp
+ *
+ *  Created on: Jul 23, 2017
+ *      Author: dan
+ */
+
+#include <CppUTest/TestHarness.h>
+
+TEST_GROUP(SodukuTests)
+{
+};
+
+
+TEST(SodukuTests, FirstTest)
+{
+	CHECK_TRUE(1 == 1);
+}
+
